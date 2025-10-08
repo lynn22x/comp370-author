8ced551a26849ecf3a4728bd4d4f49637cc1094b
Zuolong Wu <zuolong2187@gmail.com>
2025-10-08 12:28:41 -0400

    Theme extraction finished

--- scripts/collection/extract_themes.py
+++ scripts/collection/extract_themes.py
@@ -1,3 +1,6 @@
+import argparse
+import json
+
 def extract_themes(dictionary: dict):
     all_themes = []
     for item in dictionary["docs"]:
@@ -6,3 +9,21 @@ def extract_themes(dictionary: dict):
             all_themes.extend(subjects_in_book)
 
     return all_themes
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("author_json_path")
+    parser.add_argument("output_path")
+
+    args = parser.parse_args()
+
+    with open(args.author_json_path, "r") as json_data:
+        data = json.load(json_data)
+
+    all_themes = extract_themes(data)
+
+    with open(args.output_path, "w") as json_file:
+        json.dump({"themes": all_themes}, json_file)
+
+if __name__ == "__main__":
+    main()
